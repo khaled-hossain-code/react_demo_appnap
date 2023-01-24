cf2f0225c0d8270220d42dceb26d948c8b70169d
khaled hossain <md.khaled.eee@gmail.com>
2023-01-24 19:47:45 +0600

    Add Counter

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Declarative from './features/declarative/Declarative';
+
+function App() {
+  return (
+    <div className="App">
+      <Declarative />
+    </div>
+  );
+}
+
+export default App;
